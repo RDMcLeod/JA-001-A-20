95d931cbeb217c168d538dfc82ddde9ab8ea3275
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-11-19 23:41:39 -0500

    Initial Project Start

---
+++ Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jsp1;
+
+/**
+ * @author acdesigner2012
+ *
+ */
+public class JavaStartPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
