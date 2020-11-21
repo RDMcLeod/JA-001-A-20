6ab9fd55e988bfc9cd1271ea0db9a18ba1d985e1
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-11-20 20:49:59 -0500

    Recommit after gitignore
    Test with Gitignore

---
+++ Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package jsp1;
+
+/**
+ * @author acdesigner2012
+ *
+ */
+public class JavaStartPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("derrick");
+
+	  }
+
+}
+
