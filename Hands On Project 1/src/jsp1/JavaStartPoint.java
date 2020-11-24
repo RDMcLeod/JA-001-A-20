937910da3cfcde1b4150664eeda9039c3c936396
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-11-24 02:55:59 -0500

    Added Variables L10-12

--- Hands On Project 1/src/jsp1/JavaStartPoint.java
+++ Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -1,19 +1,17 @@
-/**
- * 
- */
 package jsp1;
 
-/**
- * @author acdesigner2012
- *
- */
+
 public class JavaStartPoint {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		  System.out.println("derrick");
+		int Hello_derrick = 5; 
+		double  num2 = 5.0;
+		boolean b = false;
+		System.out.println(Hello_derrick);
+		
 
 	  }
 
