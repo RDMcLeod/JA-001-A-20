12b734a0e70a6bbbe7cd7614f12843a87190987b
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-11-26 18:55:10 -0500

    Update JavaStartPoint.java

--- Hands On Project 1/src/jsp1/JavaStartPoint.java
+++ Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -1,17 +1,13 @@
 package jsp1;
 
 public class JavaStartPoint {
-        //primative 
-		public static void main(String[] args) {
-		int Hello_derrick = 9; 
-		double  num2 = 5.0;
-		boolean b = false;
-		char  c = '9' ;
-		String  str = "tim"
-		
-		System.out.println(hello_world);
+        //TODO Auto-generated method stub
+		int x = 5;
+	    int y = 7;
+	    int z = 56;
+	    int sum = x + y + z;
 	
-		
+		System.out.println(x);
 
 	  }
 
