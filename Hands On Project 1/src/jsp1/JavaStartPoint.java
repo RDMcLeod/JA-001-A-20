82ac6d278473a82b5a24523e07a529f2f9165eb8
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-11-26 19:29:37 -0500

    Update JavaStartPoint.java

--- Hands On Project 1/src/jsp1/JavaStartPoint.java
+++ Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -1,16 +1,16 @@
 package jsp1;
  
-public class JavaStartPoint {
+public class  JavaStartPoint {
  
-public static void main(String[] args) {
-	// TODO Auto-generated method stub
-    int x = 5;
-    int y = 7;
-    int z = 57;
-    double u  =  x * z;
-	double d  = Math.pow(x, y);
+      public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+           int x = 5;
+           int y = 7;
+           int z = 57;
+           double u  =  x * z;
+           double d  =  Math.pow(x, y);
 	   
-	System.out.println(d);
+           System.out.println(d);
 
 	  }
 
