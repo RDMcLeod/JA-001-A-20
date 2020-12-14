9d23291f972ad1ad5098a91973b87bc2eebe6b52
RDMcLeod <mcleod.r.derrick@gmail.com>
2020-12-14 18:17:25 -0500

    Rework - Geting back to speed
    Rework - Geting back to speed

--- Hands On Project 1/src/jsp1/JavaStartPoint.java
+++ Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -1,16 +1,16 @@
 package jsp1;
  
-public class  JavastartPoint {
+public class  JavaStartPoint {
  
       public static void main(String [ ] args) {
 	        // TODO Auto-generated method stub
            int x = 5;
            int y = 7;
-           int z = 57;
-           int u  =  x * z;
-           double d  =  Math.pow(x, y)
+           
+           
+           double d  =  Math.pow(x, y);
 	      
-          System.out.println(d)
+          System.out.println(d);
 
 	  }
 
