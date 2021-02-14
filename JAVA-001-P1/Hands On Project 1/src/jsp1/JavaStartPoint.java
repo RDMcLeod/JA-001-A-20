73467beb2c91ee800599c4aee5c9294435e82506
RDMcLeod <mcleod.r.derrick@gmail.com>
2021-02-13 19:17:43 -0500

    Project folder Clean up
    Project folder Clean up

---
+++ JAVA-001-P1/Hands On Project 1/src/jsp1/JavaStartPoint.java
@@ -0,0 +1,19 @@
+package jsp1;
+ 
+public class  JavaStartPoint {
+ 
+      public static void main(String [ ] args) {
+	        // TODO Auto-generated method stub
+           int x = 5;
+           int y = 7;
+           
+           
+           double d  =  Math.pow(x, y);
+	      
+          System.out.println(d);
+
+	  }
+
+}
+
+
---
+++ JAVA-001-P1/README.md
@@ -0,0 +1,4 @@
+# JA-001-A-20
+This is and Initial Setup for the coming structure and I will see how it goes
+THis repository has been created for the purposes of learning Java, so there wil be many iterations placed here on my journey
+THis is good article https://codegym.cc/groups/posts/why-should-you-learn-java
